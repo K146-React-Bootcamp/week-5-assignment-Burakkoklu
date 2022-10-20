5ecb3892c973123f49ea65b2fef3dc1c7d1c60b0
Ömer BurakKöklü <burakkoklu44@gmail.com>
2022-10-21 01:31:29 +0300

    Ödev
    Ödev

---
+++ my-app/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+// function Greet(){
+//     return console.log("render edildi")
+// }
+export const Greet = () => console.log("render edildi")  
+
+// export default Greet 
